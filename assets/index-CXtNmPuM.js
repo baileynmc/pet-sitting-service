a8fca4bd872cbb86464fa2253bb820058996a30f
Bailey <baileynmc@gmail.com>
2026-07-18 19:34:26 -0400

    Updates

--- assets/index-CXtNmPuM.js
+++ assets/index-CXtNmPuM.js
@@ -360,7 +360,7 @@ Error generating stack: `+e.message+`
   width: 100%;
   margin: 0;
   padding: 0;
-`,_l=Mi([{path:`/`,Component:()=>{let e=(0,ro.c)(6),t;e[0]===Symbol.for(`react.memo_cache_sentinel`)?(t=[],e[0]=t):t=e[0];let[n,r]=(0,A.useState)(t),i;e[1]===n?i=e[2]:(i={activeBookings:n,setActiveBookings:r},e[1]=n,e[2]=i);let a;e[3]===Symbol.for(`react.memo_cache_sentinel`)?(a=(0,K.jsxs)(gl,{children:[(0,K.jsx)(ml,{title:`Precious Petsitting`,logo:(0,K.jsx)(hl,{}),children:(0,K.jsx)(oc,{to:`/admin`,children:`Admin View`})}),(0,K.jsx)(`main`,{children:(0,K.jsx)(Br,{})})]}),e[3]=a):a=e[3];let o;return e[4]===i?o=e[5]:(o=(0,K.jsx)(Ec.Provider,{value:i,children:a}),e[4]=i,e[5]=o),o},children:[{index:!0,Component:_c},{path:`book`,Component:zc},{path:`admin`,Component:sl}]}],{basename:`/pet-sitting-service`}),vl=tc`
+`,_l=Mi([{path:`/`,Component:()=>{let e=(0,ro.c)(6),t;e[0]===Symbol.for(`react.memo_cache_sentinel`)?(t=[],e[0]=t):t=e[0];let[n,r]=(0,A.useState)(t),i;e[1]===n?i=e[2]:(i={activeBookings:n,setActiveBookings:r},e[1]=n,e[2]=i);let a;e[3]===Symbol.for(`react.memo_cache_sentinel`)?(a=(0,K.jsxs)(gl,{children:[(0,K.jsx)(ml,{title:`Precious Petsitting`,logo:(0,K.jsx)(hl,{}),children:(0,K.jsx)(oc,{to:`/admin`,children:`Admin View`})}),(0,K.jsx)(`main`,{children:(0,K.jsx)(Br,{})})]}),e[3]=a):a=e[3];let o;return e[4]===i?o=e[5]:(o=(0,K.jsx)(Ec.Provider,{value:i,children:a}),e[4]=i,e[5]=o),o},children:[{index:!0,Component:_c},{path:`book`,Component:zc},{path:`admin`,Component:sl}]}]),vl=tc`
   body {
     margin: 0;
     padding: 0;
